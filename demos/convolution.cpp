229969d27107cce40858827912a00e518766a8c9
Dethon <dethon@usal.es>
2015-08-24 03:58:06 +0200

    Minor changes in the convolution demo

--- demos/convolution.cpp
+++ demos/convolution.cpp
@@ -30,16 +30,10 @@ int main(int argc, char* argv[])
 	auto factory = DeviceManager::getDefaultDeviceFactory();
 	auto device = factory->openDevice();
 
-	std::shared_ptr<File> file1;
-	std::shared_ptr<File> file2;
-	std::shared_ptr<ConvolverSound> convolver;
-	std::shared_ptr<ImpulseResponse> impulseResponse;
-
-	file1 = std::make_shared<File>(argv[1]);
-	file2 = std::make_shared<File>(argv[2]);
-	impulseResponse = std::make_shared<ImpulseResponse>(std::make_shared<StreamBuffer>(file2));
-
-	convolver = std::make_shared<ConvolverSound>(file1, impulseResponse, 8);
+	std::shared_ptr<File> file1(std::make_shared<File>(argv[1]));
+	std::shared_ptr<File> file2(std::make_shared<File>(argv[2]));
+	std::shared_ptr<ImpulseResponse> impulseResponse(std::make_shared<ImpulseResponse>(std::make_shared<StreamBuffer>(file2)));
+	std::shared_ptr<ConvolverSound> convolver(std::make_shared<ConvolverSound>(file1, impulseResponse, 8));
 
 	device->lock();
 	auto handle = device->play(convolver);
