0eceac5a1d5060618e705639c46f718a38d99a44
Dethon <dethon@usal.es>
2015-09-02 23:15:44 +0200

    The convolution demo makes use of the ThreadPool class now.

--- demos/convolution.cpp
+++ demos/convolution.cpp
@@ -1,6 +1,7 @@
 #include "fx/ConvolverSound.h"
 #include "fx/ImpulseResponse.h"
 #include "util/StreamBuffer.h"
+#include "util/ThreadPool.h"
 #include "devices/DeviceManager.h"
 #include "devices/IDevice.h"
 #include "devices/IDeviceFactory.h"
@@ -30,15 +31,17 @@ int main(int argc, char* argv[])
 	auto factory = DeviceManager::getDefaultDeviceFactory();
 	auto device = factory->openDevice();
 
+	std::shared_ptr<ThreadPool> threadPool(std::make_shared<ThreadPool>(std::thread::hardware_concurrency()));
 	std::shared_ptr<File> file1(std::make_shared<File>(argv[1]));
 	std::shared_ptr<File> file2(std::make_shared<File>(argv[2]));
 	std::shared_ptr<ImpulseResponse> impulseResponse(std::make_shared<ImpulseResponse>(std::make_shared<StreamBuffer>(file2)));
-	std::shared_ptr<ConvolverSound> convolver(std::make_shared<ConvolverSound>(file1, impulseResponse, 1, 2));
+	std::shared_ptr<ConvolverSound> convolver(std::make_shared<ConvolverSound>(file1, impulseResponse, threadPool));
 
 	device->lock();
 	auto handle = device->play(convolver);
 	handle->setVolume(0.2);
 	handle->setLoopCount(-1);
+	handle->seek(95);
 	device->unlock();
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(500000));
