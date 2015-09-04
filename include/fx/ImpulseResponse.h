4d31d19f1a62420a28e7e3e2fe27033eccfb9f64
Dethon <dethon@usal.es>
2015-09-04 03:55:38 +0200

    The ImpulseResponse class now uses a FFTPlan object

--- include/fx/ImpulseResponse.h
+++ include/fx/ImpulseResponse.h
@@ -7,7 +7,8 @@
 */
 
 #include "util/StreamBuffer.h"
-#include "FFTConvolver.h"
+#include "util/FFTPlan.h"
+#include "IReader.h"
 
 #include <memory>
 #include <vector>
@@ -16,15 +17,16 @@ AUD_NAMESPACE_BEGIN
 
 /**
 * This class represents an impulse response that can be used in convolution.
-* When this class is instanced, the impulse response is divided in channels and those channels are divided in parts of FIXED_N/2 samples.
+* When this class is instanced, the impulse response is divided in channels and those channels are divided in parts of N/2 samples (N being the size of the FFT plan used).
 * The main objetive of this class is to allow the reutilization of an impulse response in various sounds without having to process it more than one time.
+* \warning The size of the FFTPlan used to process the impulse response must be the same as the one used in the convolver classes.
 */
 class AUD_API ImpulseResponse
 {
 private:
 	/**
 	* A tri-dimensional array (channels, parts, values) The impulse response is divided in channels and those channels are divided
-	* in parts of FIXED_N/2 samples. Those parts are transformed to the frequency domain transform which generates uni-dimensional 
+	* in parts of N/2 samples. Those parts are transformed to the frequency domain transform which generates uni-dimensional 
 	* arrays of fftwtf_complex data (complex numbers).
 	*/
 	std::vector<std::shared_ptr<std::vector<std::shared_ptr<std::vector<fftwf_complex>>>>> m_processedIR;
@@ -46,6 +48,14 @@ private:
 public:
 	/**
 	* Creates a new ImpulseResponse object.
+	* The impulse response will be split and transformed to the frequency domain.
+	* \param impulseResponse The impulse response sound.
+	* \param plan A shared pointer to a FFT plan used to transform the impulse response.
+	*/
+	ImpulseResponse(std::shared_ptr<StreamBuffer> impulseResponse, std::shared_ptr<FFTPlan> plan);
+
+	/**
+	* Creates a new ImpulseResponse object. This overload instances its own FFTPlan with default size.
 	* The impulse response will be split and transformed to the frequency domain.
 	* \param impulseResponse The impulse response sound.
 	*/
@@ -72,10 +82,11 @@ public:
 
 private:
 	/**
-	* Processes the impulse response sound for its use in the FFTConvolver class.
+	* Processes the impulse response sound for its use in the convovler classes.
 	* \param A shared pointer to a reader of the desired sound.
+	* \param plan A shared pointer to a FFT plan used to transform the impulse response.
 	*/
-	void processImpulseResponse(std::shared_ptr<IReader> reader);
+	void processImpulseResponse(std::shared_ptr<IReader> reader, std::shared_ptr<FFTPlan> plan);
 };
 
 AUD_NAMESPACE_END
--- src/fx/ImpulseResponse.cpp
+++ src/fx/ImpulseResponse.cpp
@@ -4,11 +4,16 @@
 #include <cstring>
 
 AUD_NAMESPACE_BEGIN
-ImpulseResponse::ImpulseResponse(std::shared_ptr<StreamBuffer> impulseResponse)
+ImpulseResponse::ImpulseResponse(std::shared_ptr<StreamBuffer> impulseResponse) :
+	ImpulseResponse(impulseResponse, std::make_shared<FFTPlan>(false))
+{
+}
+
+ImpulseResponse::ImpulseResponse(std::shared_ptr<StreamBuffer> impulseResponse, std::shared_ptr<FFTPlan> plan)
 {
 	auto reader = impulseResponse->createReader();
 	m_length = reader->getLength();
-	processImpulseResponse(impulseResponse->createReader());
+	processImpulseResponse(impulseResponse->createReader(), plan);
 }
 
 int ImpulseResponse::getNumberOfChannels()
@@ -26,51 +31,49 @@ std::shared_ptr<std::vector<std::shared_ptr<std::vector<fftwf_complex>>>> Impuls
 	return m_processedIR[n];
 }
 
-void ImpulseResponse::processImpulseResponse(std::shared_ptr<IReader> reader)
+void ImpulseResponse::processImpulseResponse(std::shared_ptr<IReader> reader, std::shared_ptr<FFTPlan> plan)
 {
 	m_channels = reader->getSpecs().channels;
+	int N = plan->getSize();
 	bool eos = false;
 	int length = reader->getLength();
 	sample_t* buffer = (sample_t*)std::malloc(length * m_channels * sizeof(sample_t));
-	int numParts = ceil((float)length / (FIXED_N / 2));
+	int numParts = ceil((float)length / (plan->getSize() / 2));
 
 	for (int i = 0; i < m_channels; i++)
 	{
 		m_processedIR.push_back(std::make_shared<std::vector<std::shared_ptr<std::vector<fftwf_complex>>>>());
 		for (int j = 0; j < numParts; j++)
-			(*m_processedIR[i]).push_back(std::make_shared<std::vector<fftwf_complex>>((FIXED_N / 2) + 1));
+			(*m_processedIR[i]).push_back(std::make_shared<std::vector<fftwf_complex>>((N / 2) + 1));
 	}
 	length += reader->getSpecs().rate;
 	reader->read(length, eos, buffer);
 
 
-	void* bufferFFT = fftwf_malloc(((FIXED_N / 2) + 1) * 2 * sizeof(fftwf_complex));
-	fftwf_plan p = fftwf_plan_dft_r2c_1d(FIXED_N, (float*)bufferFFT, (fftwf_complex*)bufferFFT, FFTW_ESTIMATE);
+	void* bufferFFT = plan->getBuffer();
 	for (int i = 0; i < m_channels; i++)
 	{
 		int partStart = 0;
 		for (int h = 0; h < numParts; h++)
 		{
 			int k = 0;
-			int len = std::min(partStart + ((FIXED_N / 2)*m_channels), length*m_channels);
-			std::memset(bufferFFT, 0, ((FIXED_N / 2) + 1) * 2 * sizeof(fftwf_complex));
+			int len = std::min(partStart + ((N / 2)*m_channels), length*m_channels);
+			std::memset(bufferFFT, 0, ((N / 2) + 1) * 2 * sizeof(fftwf_complex));
 			for (int j = partStart; j < len; j += m_channels)
 			{
 				((float*)bufferFFT)[k] = buffer[j + i];
 				k++;
 			}
-			fftwf_execute(p);
-			for (int j = 0; j < (FIXED_N / 2) + 1; j++)
+			plan->FFT(bufferFFT);
+			for (int j = 0; j < (N / 2) + 1; j++)
 			{
 				(*(*m_processedIR[i])[h])[j][0] = ((fftwf_complex*)bufferFFT)[j][0];
 				(*(*m_processedIR[i])[h])[j][1] = ((fftwf_complex*)bufferFFT)[j][1];
 			}
-			partStart += FIXED_N / 2 * m_channels;
+			partStart += N / 2 * m_channels;
 		}
 	}
-
-	fftwf_free(bufferFFT);
-	fftwf_destroy_plan(p);
+	plan->freeBuffer(bufferFFT);
 	std::free(buffer);
 }
 AUD_NAMESPACE_END
