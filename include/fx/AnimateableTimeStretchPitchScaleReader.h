424c2d7e1331fdb86ed277e610d3f8a9302fc03d
Kace <lakacey03@gmail.com>
2025-09-05 03:33:17 -0400

    remove method in header

--- include/fx/AnimateableTimeStretchPitchScaleReader.h
+++ include/fx/AnimateableTimeStretchPitchScaleReader.h
@@ -67,11 +67,6 @@ public:
 	virtual void read(int& length, bool& eos, sample_t* buffer) override;
 
 	virtual void seek(int position) override;
-
-
-	int inputReaderGetPosition() const {
-    return m_reader->getPosition();
-	}
 };
 
 AUD_NAMESPACE_END
